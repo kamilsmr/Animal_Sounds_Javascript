868c26b6a1fadde63f232a031e89c7f1b185ded2
kamilsmr <kamils6035@gmail.com>
2022-10-30 00:03:55 +0300

    finish

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-const button = document.querySelector("button");
-const bark = new Audio('sound/bark.mp3');
-button.addEventListener("click",function(){
-    let duration = bark.duration;
-    let muted = bark.muted;
-    bark.play();
-    console.log(muted);
-    
+const myArray = ["lion", "cougar", "bark"];
+        document.addEventListener("DOMContentLoaded", init);
 
-})
+        function init() {
+            myArray.forEach(function (item) {
+                let div = document.createElement("div");
+                div.setAttribute("class", "animal " + item);
+                div.innerText = item.toUpperCase();
+                div.addEventListener("click", function () {
+                    playit(item);
+                })
+                document.body.appendChild(div);
+            })
+        }
+
+        function playit(name) {
+            let activeEle = document.querySelector("." + name);
+            let sound1 = new Audio("sound/" + name + ".mp3");
+            sound1.play();
+            activeEle.classList.add("active");
+            setTimeout(function () {
+                activeEle.classList.remove("active");
+            }, 200)
+        }
